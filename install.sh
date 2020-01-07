c70bb0a3f96990437e9466c8bb0a4060fda48953
Artem Samofalov <dex157@icloud.com>
2020-01-07 03:40:10 +0300

    init commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+
+git submodule init && git submodule update
+cd ~
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/zshenv ~/.zshenv
+ln -s ~/dotfiles/zsh/prezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/zsh/zlogin ~/.zlogin
+ln -s ~/dotfiles/zsh/prezto/runcoms/zlogout ~/.zlogout
+ln -s ~/dotfiles/zsh/prezto/runcoms/zprofile ~/.zprofile
+ln -s ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/other_dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/other_dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/other_dotfiles/eslintrc ~/.eslintrc
+
+ ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+# vim +PluginInstall +qall
+
+# ~/dotfiles/vim/bundle/YouCompleteMe/install.sh
+# ~/dotfiles/vim/bundle/ctrlp-matcher/install.sh
+# cd ~/dotfiles/vim/bundle/tern_for_vim && npm install
+
+~/dotfiles/antibody/install.sh
+
+brew update
+brew upgrade reattach-to-user-namespace jq tmux
+
+# Fix ctrl-h issue, see https://github.com/christoomey/vim-tmux-navigator/issues/71
+#infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $TERM.ti
+#tic $TERM.ti
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+exit 1
